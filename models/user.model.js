9d285ce304e2bd362ba4648d9d9a0df4dacc243e
Kei <kennyy762@gmail.com>
2025-09-07 19:32:46 +0800

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const createHttpError = require('http-errors');
 const { roles } = require('../utils/constants');
 
