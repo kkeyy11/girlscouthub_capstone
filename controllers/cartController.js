3473282e5e9d2073a3ae93f7cab5e5357523c6d7
Ken <kennyy762@gmail.com>
2025-11-30 20:13:36 +0800

    Update cartController.js

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -139,7 +139,7 @@ exports.viewReservations = async (req, res) => {
     const pendingReservations = reservations.filter(r => r.status === 'Pending');
     const approvedReservations = reservations.filter(r => r.status === 'Approved');
 
-    res.render('adminReservations', { 
+    res.render('reservationList', { 
       reservations: pendingReservations, 
       approvedReservations 
     });
