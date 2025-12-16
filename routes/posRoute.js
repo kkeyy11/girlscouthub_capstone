7f48e7f3ba981909e62271e9c51bd002573c00b8
Ken <kennyy762@gmail.com>
2025-12-16 21:04:33 +0800

    update

--- routes/posRoute.js
+++ routes/posRoute.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const posController = require("../controllers/posController");
 
-
+// Render POS page
 router.get("/pos", posController.renderPOS);
-router.post("/pos/payment", posController.toPayment);
-router.post("/pos/pay", posController.processPayment);
-router.get("/pos/summary", posController.paymentSummary);
 
+// Process payment (from modal)
+router.post("/pos/pay", posController.processPayment);
+
+// Show payment summary
+router.get("/pos/summary", posController.paymentSummary);
 
 module.exports = router;
